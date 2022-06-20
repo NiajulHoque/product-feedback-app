1c9ba118bd04c6b96080976e8bd967f91f0e2205
Niajul Hoque <nh98.programmer@gmail.com>
2022-06-20 22:22:58 +0200

    update

--- src/components/FeedbackCard.tsx
+++ src/components/FeedbackCard.tsx
@@ -1,10 +1,23 @@
 import { useEffect, useState } from "react";
-// import { IFeedbackCard } from "../models/feedback.model";
+import { IFeedbackCard } from "../models/feedback.model";
 
 function FeedbackCard() {
+  const feedbackCardOne = {
+    id: 1,
+    title: "Add tags for solutions",
+    category: "Enhancement",
+    upvotes: 112,
+    status: "Suggestion",
+    description: "Easier to search for solutions based on a specific stack.",
+  };
+
   return (
     <div className="h:10% w:65%@2xs position:relative bg:gray-30">
-      Feedback Cards
+      <h3>{feedbackCardOne.title}</h3>
+      <p>{feedbackCardOne.category}</p>
+      {feedbackCardOne.upvotes}
+      {feedbackCardOne.status}
+      <p>{feedbackCardOne.description}</p>
     </div>
   );
 }
